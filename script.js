d6f3c78b06120e9d27bc1281974b3dd8cef0a258
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-04-29 13:16:51 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// const array = [1, 2, 3, 4, 5, 6, "sad", true, false, "2"];
+
+// console.log(typeof undefined);
+
+// console.log(typeof typeof null);
+
+// console.log(2);
+// console.log("as");
+// console.log(typeof 2);
+
+// console.log(countOfEvenNumbers(array));
+
+// function countOfEvenNumbers(arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] == "number" && arr[i] % 2 == 0) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+// console.log(a);
+// var a = 5;
+
+// console.log(typeof Number("1"));
+
+// console.log("a" - 1);
+
+// let a;
+// console.log(a);
+
+// console.log(null);
+
+// function sum() {
+//   let sum;
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// }
+
+// function sum(...arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+// function test(...arr) {
+//   arr[0] = 5;
+//   console.log(arr[0]);
+// }
+
+// arr = [1, 2, 3, 4];
+
+// test(arr);
+
+// console.log(arr[0]);
+
+// console.log(0.1 + 0.2);
